eb6e585e19811c63651abb14e604172995e44eba
hgs-study <hgstudy_@naver.com>
2021-12-30 14:39:05 +0900

    Initial commit

---
+++ src/main/java/com/hgstudy/HgstudyApplication.java
@@ -0,0 +1,13 @@
+package com.hgstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HgstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgstudy/HgstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HgstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
